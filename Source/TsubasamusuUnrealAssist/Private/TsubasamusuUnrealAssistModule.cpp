7d1a51bbc68fcef137cfb6ce1069e963b922b909
tsubasamusu <tsubasa_03_08@icloud.com>
2025-04-29 16:27:49 +0900

    [update] #3 FPlayerCharacterNodeFactory の登録と登録解除の処理を FTsubasamusuUnrealAssistModule に追加

--- Source/TsubasamusuUnrealAssist/Private/TsubasamusuUnrealAssistModule.cpp
+++ Source/TsubasamusuUnrealAssist/Private/TsubasamusuUnrealAssistModule.cpp
@@ -1,16 +1,41 @@
 // Copyright (c) 2025, tsubasamusu All rights reserved.
 
 #include "TsubasamusuUnrealAssistModule.h"
+#include "PlayerCharacterNodeFactory.h"
 #include "TsubasamusuEditorSettingsUtilty.h"
+
+#define LOCTEXT_NAMESPACE "FTsubasamusuUnrealAssistModule"
+#define TUA_IS_ENABLED (!IS_MONOLITHIC && !UE_BUILD_SHIPPING && !UE_BUILD_TEST && !UE_GAME && !UE_SERVER)
 
 void FTsubasamusuUnrealAssistModule::StartupModule()
 {
+#if TUA_IS_ENABLED
 	//FTsubasamusuEditorSettingsUtilty::SetupEditorSettingsForTsubasamusu();
+
+	FCoreDelegates::OnPostEngineInit.AddRaw(this, &FTsubasamusuUnrealAssistModule::RegisterPlayerCharacterNodeFactory);
+#endif
 }
 
 void FTsubasamusuUnrealAssistModule::ShutdownModule()
 {
-
+#if TUA_IS_ENABLED
+	UnregisterPlayerCharacterNodeFactory();
+#endif
 }
 
+void FTsubasamusuUnrealAssistModule::RegisterPlayerCharacterNodeFactory()
+{
+	PlayerCharacterNodeFactory = MakeShared<FPlayerCharacterNodeFactory>();
+
+	FEdGraphUtilities::RegisterVisualNodeFactory(PlayerCharacterNodeFactory);
+}
+
+void FTsubasamusuUnrealAssistModule::UnregisterPlayerCharacterNodeFactory()
+{
+	FEdGraphUtilities::UnregisterVisualNodeFactory(PlayerCharacterNodeFactory);
+
+	PlayerCharacterNodeFactory.Reset();
+}
+
+#undef LOCTEXT_NAMESPACE
 IMPLEMENT_MODULE(FTsubasamusuUnrealAssistModule, TsubasamusuUnrealAssist)
--- Source/TsubasamusuUnrealAssist/Public/TsubasamusuUnrealAssistModule.h
+++ Source/TsubasamusuUnrealAssist/Public/TsubasamusuUnrealAssistModule.h
@@ -5,12 +5,17 @@
 #include "CoreMinimal.h"
 #include "Modules/ModuleManager.h"
 
-class FToolBarBuilder;
-class FMenuBuilder;
+class FPlayerCharacterNodeFactory;
 
 class FTsubasamusuUnrealAssistModule : public IModuleInterface
 {
 public:
 	virtual void StartupModule() override;
 	virtual void ShutdownModule() override;
+
+private:
+	void RegisterPlayerCharacterNodeFactory();
+	void UnregisterPlayerCharacterNodeFactory();
+
+	TSharedPtr<FPlayerCharacterNodeFactory> PlayerCharacterNodeFactory;
 };
