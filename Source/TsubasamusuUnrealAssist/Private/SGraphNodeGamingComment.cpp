f43e91ca073411ed24705c45b568a03374a5e793
tsubasamusu <tsubasa_03_08@icloud.com>
2025-05-01 18:29:49 +0900

    [add] #3 SGraphNodeGamingComment を作成して、ゲーミング風に色が変化する処理と回転処理を追加

---
+++ Source/TsubasamusuUnrealAssist/Private/SGraphNodeGamingComment.cpp
@@ -0,0 +1,55 @@
+// Copyright (c) 2025, tsubasamusu All rights reserved.
+
+#include "SGraphNodeGamingComment.h"
+
+void SGraphNodeGamingComment::Tick(const FGeometry& AllottedGeometry, const double InCurrentTime, const float InDeltaTime)
+{
+	SGraphNodeCustomizableComment::Tick(AllottedGeometry, InCurrentTime, InDeltaTime);
+
+	UpdateGamingAnimationColor(InDeltaTime);
+	UpdateCommentNodeAngle(InDeltaTime);
+}
+
+void SGraphNodeGamingComment::Construct(const FArguments& InArgs, UEdGraphNode_Comment* InNode)
+{
+	SGraphNodeCustomizableComment::Construct(SGraphNodeCustomizableComment::FArguments(), InNode);
+}
+
+FSlateColor SGraphNodeGamingComment::GetCommentBodyColor() const
+{
+	return GetGamingAnimationColor();
+}
+
+FSlateColor SGraphNodeGamingComment::GetTitleBarColor() const
+{
+	return GetGamingAnimationColor() * CustomizableCommentNodeDefinitions::TitleBarColorMultiplier;
+}
+
+FSlateColor SGraphNodeGamingComment::GetCommentBubbleColor() const
+{
+	return GetGamingAnimationColor() * CustomizableCommentNodeDefinitions::TitleBarColorMultiplier;
+}
+
+void SGraphNodeGamingComment::UpdateGamingAnimationColor(const float InDeltaTime)
+{
+	GamingAnimationElapsedSeconds += InDeltaTime;
+	
+	if (GamingAnimationElapsedSeconds >= GamingCommentNodeDefinitions::GamingAnimationDuration)
+	{
+		GamingAnimationElapsedSeconds = 0.0f;
+	}
+}
+
+void SGraphNodeGamingComment::UpdateCommentNodeAngle(const float InDeltaTime)
+{
+	CurrentCommentNodeAngle += InDeltaTime * GamingCommentNodeDefinitions::RotatingAnglePerSeconds;
+
+	SetCommentNodeAngle(CurrentCommentNodeAngle);
+}
+
+FLinearColor SGraphNodeGamingComment::GetGamingAnimationColor() const
+{
+	const float Hue = (GamingAnimationElapsedSeconds / GamingCommentNodeDefinitions::GamingAnimationDuration) * 255.0f;
+
+	return FLinearColor::MakeFromHSV8(static_cast<uint8>(Hue), 255.0f, 255.0f);
+}
---
+++ Source/TsubasamusuUnrealAssist/Public/SGraphNodeGamingComment.h
@@ -0,0 +1,48 @@
+// Copyright (c) 2025, tsubasamusu All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SGraphNodeCustomizableComment.h"
+
+namespace GamingCommentNodeDefinitions
+{
+	static constexpr float GamingAnimationDuration = 1.0f;
+
+	static constexpr float RotatingAnglePerSeconds = 720.0f;
+}
+
+class TSUBASAMUSUUNREALASSIST_API SGraphNodeGamingComment : public SGraphNodeCustomizableComment
+{
+	typedef SGraphNodeGamingComment Super;
+
+public:
+	
+	SLATE_BEGIN_ARGS(SGraphNodeGamingComment) {}
+	SLATE_END_ARGS()
+	
+	//~ Begin SWidget Interface
+	virtual void Tick(const FGeometry& AllottedGeometry, const double InCurrentTime, const float InDeltaTime) override;
+	//~ End SWidget Interface
+	
+	void Construct(const FArguments& InArgs, UEdGraphNode_Comment* InNode);
+
+protected:
+
+	//~ Begin SGraphNodeCustomizableComment Interface
+	virtual FSlateColor GetCommentBodyColor() const override;
+	virtual FSlateColor GetTitleBarColor() const override;
+	virtual FSlateColor GetCommentBubbleColor() const override;
+	//~ End SGraphNodeCustomizableComment Interface
+	
+private:
+
+	void UpdateGamingAnimationColor(const float InDeltaTime);
+	
+	void UpdateCommentNodeAngle(const float InDeltaTime);
+	
+	FLinearColor GetGamingAnimationColor() const;
+	
+	float GamingAnimationElapsedSeconds;
+	float CurrentCommentNodeAngle;
+};
