044376ccd61d12f9562094595dcd9520efde52ae
tsubasamusu <tsubasa_03_08@icloud.com>
2025-05-02 16:38:43 +0900

    [update] #3 UTsubasamusuUnrealAssistSettings::CommentNodeType の値によって生成するコメントノードの種類を変更するという処理を FTsubasamusuNodeFactory::CreateNode() に追加

--- Source/TsubasamusuUnrealAssist/Private/TsubasamusuNodeFactory.cpp
+++ Source/TsubasamusuUnrealAssist/Private/TsubasamusuNodeFactory.cpp
@@ -1,17 +1,31 @@
 // Copyright (c) 2025, tsubasamusu All rights reserved.
 
 #include "TsubasamusuNodeFactory.h"
+#include "CommentNodeType.h"
 #include "EdGraphNode_Comment.h"
-#include "SGraphNodeCustomizableComment.h"
+#include "SGraphNodeGamingComment.h"
+#include "TsubasamusuUnrealAssistSettings.h"
 
 TSharedPtr<SGraphNode> FTsubasamusuNodeFactory::CreateNode(class UEdGraphNode* Node) const
 {
-    UEdGraphNode_Comment* CommentNode = Cast<UEdGraphNode_Comment>(Node);
+	UEdGraphNode_Comment* CommentNode = Cast<UEdGraphNode_Comment>(Node);
 
-    if (IsValid(CommentNode))
-    {
-        return SNew(SGraphNodeCustomizableComment, CommentNode);
-    }
-    
-    return nullptr;
+	if (!IsValid(CommentNode))
+	{
+		return nullptr;
+	}
+
+	const UTsubasamusuUnrealAssistSettings* Settings = UTsubasamusuUnrealAssistSettings::GetSettingsChecked();
+
+	switch (Settings->CommentNodeType)
+	{
+	case ECommentNodeType::Normal:
+		return nullptr;
+	case ECommentNodeType::Gaming:
+		return SNew(SGraphNodeGamingComment, CommentNode);
+	case ECommentNodeType::PONG:
+		return nullptr;
+	default:
+		return nullptr;
+	}
 }
