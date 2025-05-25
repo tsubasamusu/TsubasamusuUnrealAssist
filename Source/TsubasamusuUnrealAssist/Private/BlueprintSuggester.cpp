ed5e53f567ef0653c6b7e8f73788a152a4c17f0a
tsubasamusu <tsubasa_03_08@icloud.com>
2025-05-25 15:48:31 +0900

    [add] #13 FBlueprintSuggester を作成して OnNodeAdded() を追加

---
+++ Source/TsubasamusuUnrealAssist/Private/BlueprintSuggester.cpp
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, tsubasamusu All rights reserved.
+
+#include "BlueprintSuggester.h"
+
+void FBlueprintSuggester::OnNodeAdded(UEdGraphNode* AddedNode)
+{
+	
+}
---
+++ Source/TsubasamusuUnrealAssist/Public/BlueprintSuggester.h
@@ -0,0 +1,12 @@
+// Copyright (c) 2025, tsubasamusu All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+class TSUBASAMUSUUNREALASSIST_API FBlueprintSuggester final
+{
+public:
+	
+	void OnNodeAdded(UEdGraphNode* AddedNode);
+};
