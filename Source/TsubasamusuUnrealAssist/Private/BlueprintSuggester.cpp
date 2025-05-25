0beca87d35b7c190976e3478c095e8b642604654
tsubasamusu <tsubasa_03_08@icloud.com>
2025-05-25 17:18:46 +0900

    [update] #13 ノードが追加されて一定時間経過してから任意の処理を実行できるように修正

--- Source/TsubasamusuUnrealAssist/Private/BlueprintSuggester.cpp
+++ Source/TsubasamusuUnrealAssist/Private/BlueprintSuggester.cpp
@@ -4,5 +4,46 @@
 
 void FBlueprintSuggester::OnNodeAdded(UEdGraphNode* AddedNode)
 {
+	const TWeakObjectPtr<UEdGraphNode> WeakAddedNode = AddedNode;
+	CachedWeakAddedNodes.AddUnique(WeakAddedNode);
+
+	if (bIsWaitingSuggestion)
+	{
+		return;
+	}
+	
+	const TWeakPtr<FBlueprintSuggester> WeakBlueprintSuggester = SharedThis(this);
+
+	WaitSuggestionHandle = FTSTicker::GetCoreTicker().AddTicker(FTickerDelegate::CreateLambda([WeakBlueprintSuggester](const float DeltaTime)
+	{
+		if (!WeakBlueprintSuggester.IsValid())
+		{
+			return false;
+		}
+
+		const TSharedPtr<FBlueprintSuggester> PinnedBlueprintSuggester = WeakBlueprintSuggester.Pin();
+		
+		PinnedBlueprintSuggester->CurrentWaitSuggestionSeconds += DeltaTime;
+
+		if (PinnedBlueprintSuggester->CurrentWaitSuggestionSeconds < PinnedBlueprintSuggester->MaxWaitSuggestionSeconds)
+		{
+			return true;
+		}
+
+		FTSTicker::GetCoreTicker().RemoveTicker(PinnedBlueprintSuggester->WaitSuggestionHandle);
+		PinnedBlueprintSuggester->bIsWaitingSuggestion = false;
+		PinnedBlueprintSuggester->CurrentWaitSuggestionSeconds = 0.0f;
+
+		PinnedBlueprintSuggester->OnNodesAdded(PinnedBlueprintSuggester->CachedWeakAddedNodes);
+		PinnedBlueprintSuggester->CachedWeakAddedNodes.Empty();
+
+		return false;
+	}));
+
+	bIsWaitingSuggestion = true;
+}
+
+void FBlueprintSuggester::OnNodesAdded(TArray<TWeakObjectPtr<UEdGraphNode>> WeakAddedNodes)
+{
 	
 }
--- Source/TsubasamusuUnrealAssist/Public/BlueprintSuggester.h
+++ Source/TsubasamusuUnrealAssist/Public/BlueprintSuggester.h
@@ -4,9 +4,22 @@
 
 #include "CoreMinimal.h"
 
-class TSUBASAMUSUUNREALASSIST_API FBlueprintSuggester final
+class TSUBASAMUSUUNREALASSIST_API FBlueprintSuggester final : public TSharedFromThis<FBlueprintSuggester>
 {
 public:
 	
 	void OnNodeAdded(UEdGraphNode* AddedNode);
+
+private:
+
+	void OnNodesAdded(TArray<TWeakObjectPtr<UEdGraphNode>> WeakAddedNodes);
+
+	TArray<TWeakObjectPtr<UEdGraphNode>> CachedWeakAddedNodes;
+
+	FTSTicker::FDelegateHandle WaitSuggestionHandle;
+	
+	bool bIsWaitingSuggestion;
+	
+	float CurrentWaitSuggestionSeconds;
+	const float MaxWaitSuggestionSeconds = 1.0f;
 };
