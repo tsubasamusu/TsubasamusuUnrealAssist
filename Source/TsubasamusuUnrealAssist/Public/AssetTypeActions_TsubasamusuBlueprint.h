545bc06f02c32538f5f3e21904ddec22daa7683f
tsubasamusu <tsubasa_03_08@icloud.com>
2025-07-04 17:28:02 +0900

    [update] #13 ブループリントを開いた時に FTsubasamusuBlueprintEditor をそのブループリントに関連付けるように修正

--- Source/TsubasamusuUnrealAssist/Public/AssetTypeActions_TsubasamusuBlueprint.h
+++ Source/TsubasamusuUnrealAssist/Public/AssetTypeActions_TsubasamusuBlueprint.h
@@ -8,7 +8,12 @@
 class FAssetTypeActions_TsubasamusuBlueprint : public FAssetTypeActions_Blueprint
 {
 public:
+	
 	//~ Begin IAssetTypeActions Interface
 	virtual void OpenAssetEditor(const TArray<UObject*>& InObjects, TSharedPtr<class IToolkitHost> EditWithinLevelEditor = TSharedPtr<IToolkitHost>()) override;
 	//~ End IAssetTypeActions Interface
+
+private:
+	
+	static bool ShouldUseDataOnlyEditor(const UBlueprint* Blueprint);
 };
