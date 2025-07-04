9948fa4c5e55361b567623dc2f7b2d57ef41f8e7
tsubasamusu <tsubasa_03_08@icloud.com>
2025-07-04 15:59:14 +0900

    [update] #13 プラグインの依存関係に Kismet を追加

--- Source/TsubasamusuUnrealAssist/TsubasamusuUnrealAssist.Build.cs
+++ Source/TsubasamusuUnrealAssist/TsubasamusuUnrealAssist.Build.cs
@@ -29,7 +29,8 @@ public class TsubasamusuUnrealAssist : ModuleRules
             "HTTP",
             "Json",
             "JsonUtilities",
-            "AssetTools"
+            "AssetTools",
+            "Kismet"
         });
 	}
 }
