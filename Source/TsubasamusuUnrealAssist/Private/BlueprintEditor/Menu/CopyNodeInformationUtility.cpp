b5ff36430a753360b49beaa4c2a2c3f3b342b38b
tsubasamusu <tsubasa_03_08@icloud.com>
2025-08-10 08:33:50 +0900

    [add] #21 FCopyNodeInformationUtility を作成して AddCopyNodeInformationMenu() を追加

---
+++ Source/TsubasamusuUnrealAssist/Private/BlueprintEditor/Menu/CopyNodeInformationUtility.cpp
@@ -0,0 +1,33 @@
+// Copyright (c) 2025, tsubasamusu All rights reserved.
+
+#include "BlueprintEditor/Menu/CopyNodeInformationUtility.h"
+#include  "BlueprintEditor/GraphNodeUtility.h"
+
+#define LOCTEXT_NAMESPACE "FCopyNodeInformationUtility"
+
+void FCopyNodeInformationUtility::AddCopyNodeInformationMenu(const TWeakObjectPtr<UEdGraph> InGraph, FMenuBuilder& InMenuBuilder)
+{
+	const FName ExtensionHookName = TEXT("TsubasamusuUnrealAssistSection");
+	const TAttribute HeadingText = LOCTEXT("TsubasamusuUnrealAssistSectionHeader", "Tsubasamusu Unreal Assist");
+    
+	InMenuBuilder.BeginSection(ExtensionHookName, HeadingText);
+
+	const FText LabelText = LOCTEXT("CreateArrayNodeLabelText", "Copy in JSON format");
+	const FText ToolTipText = LOCTEXT("CreateArrayNodeToolTipText", "Copy selected nodes in JSON format.");
+
+	const FSlateIcon MenuIcon = FSlateIcon(FAppStyle::GetAppStyleSetName(), "GenericCommands.Copy");
+
+	InMenuBuilder.AddMenuEntry(LabelText, ToolTipText, MenuIcon, FUIAction(FExecuteAction::CreateLambda([InGraph]()
+	{
+		if (InGraph.IsValid())
+		{
+			const TArray<UEdGraphNode*> SelectedNodes = FGraphNodeUtility::GetSelectedNodes(InGraph.Get());
+		
+			//TODO: Copy selected nodes information in JSON format
+		}
+	})));
+	
+	InMenuBuilder.EndSection();
+}
+
+#undef LOCTEXT_NAMESPACE
