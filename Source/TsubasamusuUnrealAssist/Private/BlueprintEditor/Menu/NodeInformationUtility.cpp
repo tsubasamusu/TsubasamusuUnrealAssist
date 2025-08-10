34c3ab62e7d26fcc0f7f0e0cc09479004b5bb462
tsubasamusu <tsubasa_03_08@icloud.com>
2025-08-10 09:10:31 +0900

    [add] #21 FNodeInformationUtility を作成して必要な関数を追加

---
+++ Source/TsubasamusuUnrealAssist/Private/BlueprintEditor/Menu/NodeInformationUtility.cpp
@@ -0,0 +1,82 @@
+// Copyright (c) 2025, tsubasamusu All rights reserved.
+
+#include "BlueprintEditor/Menu/NodeInformationUtility.h"
+#include "EdGraphNode_Comment.h"
+#include "BlueprintEditor/Menu/CommentGeneration/NodeData.h"
+
+FNodeDataList FNodeInformationUtility::GetNodeDataList(const TArray<UEdGraphNode*>& InNodes)
+{
+	TArray<FNodeData> NodesDataList;
+
+	for (const UEdGraphNode* Node : InNodes)
+	{
+		FNodeData NodeData =
+		{
+			.NodeName = Node->GetNodeTitle(ENodeTitleType::FullTitle).ToString(),
+			.Comment = Node->NodeComment,
+			.bIsCommentNode = IsCommentNode(Node),
+			.PinDataList = GetPinDataList(Node)
+		};
+
+		NodesDataList.Add(NodeData);
+	}
+
+	return FNodeDataList(NodesDataList);
+}
+
+TArray<FPinData> FNodeInformationUtility::GetPinDataList(const UEdGraphNode* InNode)
+{
+	TArray<FPinData> PinDataList;
+
+	for (const UEdGraphPin* Pin : InNode->GetAllPins())
+	{
+		FPinData PinData
+		{
+			.PinName = Pin->GetDisplayName().IsEmpty() ? Pin->PinName.ToString() : Pin->GetDisplayName().ToString(),
+			.PinType = GetPinTypeAsString(Pin),
+			.PinId = Pin->PinId.ToString(),
+			.DefaultValue = Pin->GetDefaultAsString(),
+			.bDefaultValueIsUsed = IsPinUsesDefaultValue(Pin),
+			.ConnectedPinIds = GetPinIds(Pin->LinkedTo)
+		};
+
+		PinDataList.Add(PinData);
+	}
+
+	return PinDataList;
+}
+
+TArray<FString> FNodeInformationUtility::GetPinIds(const TArray<UEdGraphPin*>& InPins)
+{
+	TArray<FString> PinIds;
+
+	for (const UEdGraphPin* Pin : InPins)
+	{
+		PinIds.Add(Pin->PinId.ToString());
+	}
+
+	return PinIds;
+}
+
+FString FNodeInformationUtility::GetPinTypeAsString(const UEdGraphPin* InPin)
+{
+	switch (InPin->Direction)
+	{
+	case EGPD_Input:
+		return TEXT("Input");
+	case EGPD_Output:
+		return TEXT("Output");
+	default:
+		return TEXT("UnknownType");
+	}
+}
+
+bool FNodeInformationUtility::IsPinUsesDefaultValue(const UEdGraphPin* InPin)
+{
+	return !InPin->HasAnyConnections() && InPin->Direction == EGPD_Input;
+}
+
+bool FNodeInformationUtility::IsCommentNode(const UEdGraphNode* InNode)
+{
+	return IsValid(Cast<UEdGraphNode_Comment>(InNode));
+}
