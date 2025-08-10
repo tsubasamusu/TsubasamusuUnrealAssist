64800d9450dbd899f2ecc02cc1d63406e939a1b7
tsubasamusu <tsubasa_03_08@icloud.com>
2025-08-10 09:11:20 +0900

    [update] #21 FCommentGenerationUtility から不要な関数を削除

--- Source/TsubasamusuUnrealAssist/Private/BlueprintEditor/Menu/CommentGenerationUtility.cpp
+++ Source/TsubasamusuUnrealAssist/Private/BlueprintEditor/Menu/CommentGenerationUtility.cpp
@@ -4,6 +4,7 @@
 #include "EdGraphNode_Comment.h"
 #include "HttpModule.h"
 #include "JsonObjectConverter.h"
+#include "BlueprintEditor/Menu/NodeInformationUtility.h"
 #include "BlueprintEditor/Menu/CommentGeneration/GptResponse.h"
 #include "BlueprintEditor/Menu/CommentGeneration/NodeData.h"
 #include "Debug/TsubasamusuLogUtility.h"
@@ -65,60 +66,6 @@ void FCommentGenerationUtility::UpdateCommentByGpt(const TWeakObjectPtr<UEdGraph
 	});
 }
 
-FNodeDataList FCommentGenerationUtility::GetNodeDataList(const TArray<UEdGraphNode*>& InNodes)
-{
-	TArray<FNodeData> NodesDataList;
-
-	for (const UEdGraphNode* Node : InNodes)
-	{
-		FNodeData NodeData =
-		{
-			.NodeName = Node->GetNodeTitle(ENodeTitleType::FullTitle).ToString(),
-			.Comment = Node->NodeComment,
-			.bIsCommentNode = IsCommentNode(Node),
-			.PinDataList = GetPinDataList(Node)
-		};
-
-		NodesDataList.Add(NodeData);
-	}
-
-	return FNodeDataList(NodesDataList);
-}
-
-TArray<FPinData> FCommentGenerationUtility::GetPinDataList(const UEdGraphNode* InNode)
-{
-	TArray<FPinData> PinDataList;
-
-	for (const UEdGraphPin* Pin : InNode->GetAllPins())
-	{
-		FPinData PinData
-		{
-			.PinName = Pin->GetDisplayName().IsEmpty() ? Pin->PinName.ToString() : Pin->GetDisplayName().ToString(),
-			.PinType = GetPinTypeAsString(Pin),
-			.PinId = Pin->PinId.ToString(),
-			.DefaultValue = Pin->GetDefaultAsString(),
-			.bDefaultValueIsUsed = IsPinUsesDefaultValue(Pin),
-			.ConnectedPinIds = GetPinIds(Pin->LinkedTo)
-		};
-
-		PinDataList.Add(PinData);
-	}
-
-	return PinDataList;
-}
-
-TArray<FString> FCommentGenerationUtility::GetPinIds(const TArray<UEdGraphPin*>& InPins)
-{
-	TArray<FString> PinIds;
-
-	for (const UEdGraphPin* Pin : InPins)
-	{
-		PinIds.Add(Pin->PinId.ToString());
-	}
-
-	return PinIds;
-}
-
 TArray<UEdGraphNode*> FCommentGenerationUtility::GetActiveNodes(const TArray<UEdGraphNode*>& InNodes)
 {
 	const UTsubasamusuUnrealAssistSettings* TsubasamusuUnrealAssistSettings = UTsubasamusuUnrealAssistSettings::GetSettingsChecked();
@@ -134,7 +81,7 @@ TArray<UEdGraphNode*> FCommentGenerationUtility::GetActiveNodes(const TArray<UEd
 			continue;
 		}
 
-		if (IsCommentNode(Node))
+		if (FNodeInformationUtility::IsCommentNode(Node))
 		{
 			if (!TsubasamusuUnrealAssistSettings->bIgnoreCommentNodes)
 			{
@@ -168,16 +115,6 @@ bool FCommentGenerationUtility::HasConnectedPins(const UEdGraphNode* InNode)
 	return false;
 }
 
-bool FCommentGenerationUtility::IsCommentNode(const UEdGraphNode* InNode)
-{
-	return IsValid(Cast<UEdGraphNode_Comment>(InNode));
-}
-
-bool FCommentGenerationUtility::IsPinUsesDefaultValue(const UEdGraphPin* InPin)
-{
-	return !InPin->HasAnyConnections() && InPin->Direction == EGPD_Input;
-}
-
 int32 FCommentGenerationUtility::GetCharNum(const FString& InString, const TCHAR& InChar)
 {
 	int32 CharNum = 0;
@@ -191,19 +128,6 @@ int32 FCommentGenerationUtility::GetCharNum(const FString& InString, const TCHAR
 	}
 
 	return CharNum;
-}
-
-FString FCommentGenerationUtility::GetPinTypeAsString(const UEdGraphPin* InPin)
-{
-	switch (InPin->Direction)
-	{
-	case EGPD_Input:
-		return TEXT("Input");
-	case EGPD_Output:
-		return TEXT("Output");
-	default:
-		return TEXT("UnknownType");
-	}
 }
 
 void FCommentGenerationUtility::GenerateComment(const FString& NodeDataListString, const TFunction<void(const bool bSucceeded, const FString& Message)>& OnGeneratedComment)
@@ -353,7 +277,7 @@ bool FCommentGenerationUtility::TryGetNodeDataListStringUnderComment(FString& Ou
 		return false;
 	}
 
-	const FNodeDataList NodeDataList = GetNodeDataList(ActiveNodesUnderComment);
+	const FNodeDataList NodeDataList = FNodeInformationUtility::GetNodeDataList(ActiveNodesUnderComment);
 	return FJsonObjectConverter::UStructToJsonObjectString(NodeDataList, OutNodeDataListString, 0, 0);
 }
 
