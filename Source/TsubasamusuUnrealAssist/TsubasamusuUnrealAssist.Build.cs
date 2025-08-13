7e902f6715ae40b0582fad79601ed3820c995722
tsubasamusu <tsubasa_03_08@icloud.com>
2025-08-13 14:30:08 +0900

    [update] #23 プラグインの依存関係に AssetRegistry を追加

--- Source/TsubasamusuUnrealAssist/TsubasamusuUnrealAssist.Build.cs
+++ Source/TsubasamusuUnrealAssist/TsubasamusuUnrealAssist.Build.cs
@@ -35,7 +35,8 @@ public class TsubasamusuUnrealAssist : ModuleRules
             "ApplicationCore",
             "ToolMenus",
             "ContentBrowserData",
-            "ContentBrowserAssetDataSource"
-        });
+            "ContentBrowserAssetDataSource",
+            "AssetRegistry"
+		});
 	}
 }
