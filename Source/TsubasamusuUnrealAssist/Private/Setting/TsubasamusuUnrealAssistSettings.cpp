b15ef50adb67e225cc8e6528826357a4f2c612da
tsubasamusu <tsubasa_03_08@icloud.com>
2026-01-30 07:37:56 +0900

    #23 不要なエディタ設定項目を削除

--- Source/TsubasamusuUnrealAssist/Private/Setting/TsubasamusuUnrealAssistSettings.cpp
+++ Source/TsubasamusuUnrealAssist/Private/Setting/TsubasamusuUnrealAssistSettings.cpp
@@ -5,29 +5,8 @@
 
 UTsubasamusuUnrealAssistSettings::UTsubasamusuUnrealAssistSettings(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
 {
-	// General
-	CommentNodeType = ECommentNodeType::Normal;
-
-	// Node Suggestion
-	bEnableNodeSuggestion = false;
-
 	// Comment Translation
 	DeeplApiKey = TEXT("");
-
-	// Gaming Comment Node
-	GamingColorAnimationDuration = 1.0f;
-	GamingScaleAnimationDuration = 1.0f;
-	GamingCommentNodeRotatingAnglePerSeconds = 720.0f;
-	MaxGamingCommentNodeScale = 1.5f;
-
-	// PONG Comment Node
-	PongCommentNodeColor = FLinearColor::Black;
-	PongUiColor = FLinearColor::White;
-	PongBallImageSize = FVector2D(30.0f, 30.0f);
-	PongSliderLengthMultiplier = 0.3f;
-	PongBallSpeed = 100.0f;
-	PongIntervalSeconds = 1.0f;
-	bMakeLeftScrollBarNPC = false;
 
 	// Comment Generation
 	OpenAiApiKey = TEXT("");
