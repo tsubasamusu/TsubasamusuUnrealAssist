b267f289db4adbbdcd1ea30b1fd566c625ebe9f9
tsubasamusu <tsubasa_03_08@icloud.com>
2026-02-25 09:31:16 +0900

    #42 FNodePreviewer に FindParentWidget() を追加

--- Source/TsubasamusuUnrealAssist/Private/Blueprint/NodePreviewer.h
+++ Source/TsubasamusuUnrealAssist/Private/Blueprint/NodePreviewer.h
@@ -32,6 +32,22 @@ private:
 	static TSharedPtr<SWidget> FindWidgetDisplayingToolTipFromNodeSelectionWidget(const TSharedPtr<SWidget> InNodeSelectionWidget);
 	static TSharedPtr<SInlineEditableTextBlock> FindParentInlineEditableTextBlock(const TSharedPtr<SWidget> InWidget);
 	
+	template<typename WidgetClass>
+	static TSharedPtr<WidgetClass> FindParentWidget(const TSharedPtr<SWidget> InWidget, const FName& WidgetTypeToFind)
+	{
+		static_assert(TIsDerivedFrom<WidgetClass, SWidget>::Value, "WidgetClass must be derived from SWidget.");
+
+		for (TSharedPtr<SWidget> Widget = InWidget; Widget.IsValid(); Widget = Widget->GetParentWidget())
+		{
+			if (Widget->GetType() == WidgetTypeToFind)
+			{
+				return StaticCastSharedPtr<WidgetClass>(Widget);
+			}
+		}
+
+		return nullptr;
+	}
+	
 	TWeakPtr<SWidget> WidgetWhoseToolTipWasPreviouslyEdited;
 	TWeakPtr<SToolTip> LastBeforeEditingToolTipWidget;
 	
