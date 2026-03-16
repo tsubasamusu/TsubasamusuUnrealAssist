4c0c3de14b9480a5681c6d6d40193845502e886a
tsubasamusu <tsubasa_03_08@icloud.com>
2026-03-16 14:57:17 +0900

    #59 ULlamaServerOption_Temperature を定義

--- Source/TsubasamusuUnrealAssist/Private/LlamaServerOption/LlamaServerOption.cpp
+++ Source/TsubasamusuUnrealAssist/Private/LlamaServerOption/LlamaServerOption.cpp
@@ -125,3 +125,23 @@ bool ULlamaServerOption_EnableWebUi::IsValidArgument() const
 {
 	return true;
 }
+
+FString ULlamaServerOption_Temperature::GetParameter() const
+{
+	return TEXT("temperature");
+}
+
+void ULlamaServerOption_Temperature::SetArgument(const FString& InArgument)
+{
+	Temperature = FCString::Atof(*InArgument);
+}
+
+FString ULlamaServerOption_Temperature::GetArgument() const
+{
+	return FString::SanitizeFloat(Temperature);
+}
+
+bool ULlamaServerOption_Temperature::IsValidArgument() const
+{
+	return 0.f <= Temperature && Temperature <= 1.f;
+}
