ca728e1f20d787f9089e5e313e161fa7c4ebe172
tsubasamusu <tsubasa_03_08@icloud.com>
2026-03-17 16:32:42 +0900

    #59 FCommentGenerator から GenerateComment() と TryGetGptRequestString() を削除

--- Source/TsubasamusuUnrealAssist/Private/Blueprint/CommentGenerator.h
+++ Source/TsubasamusuUnrealAssist/Private/Blueprint/CommentGenerator.h
@@ -13,12 +13,10 @@ public:
 
 private:
 	static void UpdateCommentByGpt(const TWeakObjectPtr<UEdGraphNode_Comment> InCommentNode);
-	static void GenerateComment(const FString& InNodeDataListString, const TFunction<void(const bool bInSucceeded, const FString& InMessage)>& InGeneratedCommentFunction);
 	
 	static TArray<UEdGraphNode*> GetActiveNodes(const TArray<UEdGraphNode*>& InNodes);
 	static bool HasConnectedPins(const UEdGraphNode* InNode);
 	
-	static bool TryGetGptRequestString(const FString& InNodeDataListString, FString& OutGptRequestString);
 	static FString GetDesiredPrompt(const FString& InNodeDataListString);
 
 	static TArray<UEdGraphNode*> GetNodesUnderComment(const TWeakObjectPtr<UEdGraphNode_Comment> InCommentNode);
