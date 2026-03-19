86e523d79cb33c7af9b230a1550d4aab7fd2fe6b
tsubasamusu <tsubasa_03_08@icloud.com>
2026-03-20 08:27:52 +0900

    #59 FNodeUtility の一部の静的関数を FArrayNodeCreator に移動

--- Source/TsubasamusuUnrealAssist/Private/Blueprint/ArrayNodeCreator.cpp
+++ Source/TsubasamusuUnrealAssist/Private/Blueprint/ArrayNodeCreator.cpp
@@ -98,7 +98,139 @@ UK2Node_MakeArray* FArrayNodeCreator::CreateArrayNode(const TWeakObjectPtr<UEdGr
 	return CreatedArrayNode;
 }
 
-FIntPoint FArrayNodeCreator::GetDesiredArrayNodePosition(const TArray<UEdGraphNode*>& InNodes)
+TArray<UEdGraphPin*> FArrayNodeCreator::GetOutputPins(const TWeakObjectPtr<UEdGraphNode> InNode)
+{
+	TArray<UEdGraphPin*> OutputPins;
+
+	if (InNode.IsValid())
+	{
+		for (UEdGraphPin* Pin : InNode->GetAllPins())
+		{
+			if (Pin && Pin->Direction == EGPD_Output && !Pin->GetDisplayName().IsEmpty())
+			{
+				OutputPins.Add(Pin);
+			}
+		}
+	}
+	
+	return OutputPins;
+}
+
+TArray<UEdGraphPin*> FArrayNodeCreator::GetOutputPins(const TWeakObjectPtr<UEdGraphNode> InNode, const FEdGraphPinType& InSpecificPinType)
+{
+	const TArray<UEdGraphPin*> OutputPins = GetOutputPins(InNode);
+	
+	const auto IsSameTypePin = [InSpecificPinType](const UEdGraphPin* InPin)
+	{
+		return InPin && InPin->PinType == InSpecificPinType;
+	};
+	
+	return OutputPins.FilterByPredicate(IsSameTypePin);
+}
+
+TArray<UEdGraphPin*> FArrayNodeCreator::GatherOutputPins(const TArray<TWeakObjectPtr<UEdGraphNode>>& InNodes)
+{
+	TArray<UEdGraphPin*> OutputPins;
+
+	for (const TWeakObjectPtr<UEdGraphNode>& Node : InNodes)
+	{
+		const TArray<UEdGraphPin*> NodeOutputPins = GetOutputPins(Node);
+		OutputPins.Append(NodeOutputPins);
+	}
+
+	return OutputPins;
+}
+
+TArray<UEdGraphPin*> FArrayNodeCreator::GatherOutputPins(const TArray<TWeakObjectPtr<UEdGraphNode>>& InNodes,const FEdGraphPinType& InSpecificPinType)
+{
+	const TArray<UEdGraphPin*> OutputPins = GatherOutputPins(InNodes);
+	
+	const auto IsSameTypePin = [InSpecificPinType](const UEdGraphPin* InPin)
+	{
+		return InPin && InPin->PinType == InSpecificPinType;
+	};
+	
+	return OutputPins.FilterByPredicate(IsSameTypePin);
+}
+
+bool FArrayNodeCreator::TryGetNodesOutputPinsSameType(const TArray<TWeakObjectPtr<UEdGraphNode>>& InNodes, FEdGraphPinType& OutNodesOutputPinsSameType)
+{
+	const TArray<UEdGraphPin*> OutputPins = GatherOutputPins(InNodes);
+	
+	for (const UEdGraphPin* OutputPin : OutputPins)
+	{
+		if (OutputPin)
+		{
+			const FEdGraphPinType& OutputPinType = OutputPin->PinType;
+			
+			const auto IsNodeHasSameTypeOutputPins = [OutputPinType](const TWeakObjectPtr<UEdGraphNode>& InNode)
+			{
+				return InNode.IsValid() && !GetOutputPins(InNode, OutputPinType).IsEmpty();
+			};
+		
+			const TArray<TWeakObjectPtr<UEdGraphNode>> NodesHasSameTypeOutputPins = InNodes.FilterByPredicate(IsNodeHasSameTypeOutputPins);
+		
+			if (NodesHasSameTypeOutputPins.Num() == InNodes.Num())
+			{
+				OutNodesOutputPinsSameType = OutputPinType;
+				return true;
+			}
+		}
+	}
+
+	return false;
+}
+
+bool FArrayNodeCreator::IsExecutionPinType(const FEdGraphPinType& InPinType)
+{
+	return InPinType.PinCategory == UEdGraphSchema_K2::PC_Exec;
+}
+
+void FArrayNodeCreator::SortPinsByPositionY(TArray<UEdGraphPin*>& OutPins)
+{
+	TArray<int32> OutPinsIndexes;
+	OutPinsIndexes.SetNum(OutPins.Num());
+
+	for (int32 i = 0; i < OutPinsIndexes.Num(); i++)
+	{
+		OutPinsIndexes[i] = i;
+	}
+
+	OutPinsIndexes.StableSort([&OutPins](const int32 InFirstIndex, const int32 InSecondIndex)
+	{
+		const int32 FirstPinPositionY = GetPinPosition(OutPins[InFirstIndex]).Y;
+		const int32 SecondPinPositionY = GetPinPosition(OutPins[InSecondIndex]).Y;
+		
+		return FirstPinPositionY < SecondPinPositionY;
+	});
+
+	TArray<UEdGraphPin*> SortedPins;
+	SortedPins.SetNum(OutPins.Num());
+	
+	for (int32 i = 0; i < SortedPins.Num(); i++)
+	{
+		SortedPins[i] = OutPins[OutPinsIndexes[i]];
+	}
+	
+	OutPins = SortedPins;
+}
+
+FIntPoint FArrayNodeCreator::GetPinPosition(const UEdGraphPin* InPin)
+{
+	if (InPin)
+	{
+		const UEdGraphNode* PinNode = InPin->GetOwningNode();
+
+		if (IsValid(PinNode))
+		{
+			return FIntPoint(PinNode->NodePosX, PinNode->NodePosY);
+		}
+	}
+	
+	return FIntPoint();
+}
+
+FIntPoint FArrayNodeCreator::GetDesiredArrayNodePosition(const TArray<TWeakObjectPtr<UEdGraphNode>>& InNodes)
 {
 	const int32 DesiredArrayNodePositionX = GetNodesMaxPositionX(InNodes) + 200;
 	const int32 DesiredArrayNodePositionY = GetNodesAveragePositionY(InNodes);
@@ -106,40 +238,37 @@ FIntPoint FArrayNodeCreator::GetDesiredArrayNodePosition(const TArray<UEdGraphNo
 	return FIntPoint(DesiredArrayNodePositionX, DesiredArrayNodePositionY);
 }
 
-int32 FArrayNodeCreator::GetNodesMaxPositionX(const TArray<UEdGraphNode*>& InNodes)
+int32 FArrayNodeCreator::GetNodesMaxPositionX(const TArray<TWeakObjectPtr<UEdGraphNode>>& InNodes)
 {
 	int32 MaxPositionX = 0;
 
-	for (const UEdGraphNode* Node : InNodes)
+	for (const TWeakObjectPtr<UEdGraphNode>& Node : InNodes)
 	{
-		if (!IsValid(Node))
+		if (Node.IsValid())
 		{
-			continue;
-		}
-		
-		if (MaxPositionX == 0)
-		{
-			MaxPositionX = Node->NodePosX;
+			if (MaxPositionX == 0)
+			{
+				MaxPositionX = Node->NodePosX;
+				continue;
+			}
 
-			continue;
-		}
-
-		if (Node->NodePosX > MaxPositionX)
-		{
-			MaxPositionX = Node->NodePosX;
+			if (Node->NodePosX > MaxPositionX)
+			{
+				MaxPositionX = Node->NodePosX;
+			}
 		}
 	}
 
 	return MaxPositionX;
 }
 
-int32 FArrayNodeCreator::GetNodesAveragePositionY(const TArray<UEdGraphNode*>& InNodes)
+int32 FArrayNodeCreator::GetNodesAveragePositionY(const TArray<TWeakObjectPtr<UEdGraphNode>>& InNodes)
 {
 	int32 TotalPositionY = 0;
 	
-	for (const UEdGraphNode* Node : InNodes)
+	for (const TWeakObjectPtr<UEdGraphNode>& Node : InNodes)
 	{
-		if (IsValid(Node))
+		if (Node.IsValid())
 		{
 			TotalPositionY += Node->NodePosY;
 		}
--- Source/TsubasamusuUnrealAssist/Private/Blueprint/ArrayNodeCreator.h
+++ Source/TsubasamusuUnrealAssist/Private/Blueprint/ArrayNodeCreator.h
@@ -10,11 +10,24 @@ class FArrayNodeCreator final
 {
 public:
 	static void AddCreateArrayNodeMenu(const TWeakObjectPtr<UEdGraph> InGraph, FMenuBuilder& InMenuBuilder, const TSharedPtr<const FEdGraphPinType> InArrayNodePinType);
+	
+private:
 	static UK2Node_MakeArray* CreateArrayNode(const TWeakObjectPtr<UEdGraph> InGraph, const TArray<UEdGraphNode*>& InNodes, const FEdGraphPinType& InArrayNodePinType);
 
-private:
-	static FIntPoint GetDesiredArrayNodePosition(const TArray<UEdGraphNode*>& InNodes);
+	static TArray<UEdGraphPin*> GetInputPins(const UEdGraphNode* InNode);
+	
+	static TArray<UEdGraphPin*> GetOutputPins(const TWeakObjectPtr<UEdGraphNode> InNode);
+	static TArray<UEdGraphPin*> GetOutputPins(const TWeakObjectPtr<UEdGraphNode> InNode, const FEdGraphPinType& InSpecificPinType);
+	
+	static TArray<UEdGraphPin*> GatherOutputPins(const TArray<TWeakObjectPtr<UEdGraphNode>>& InNodes);
+	static TArray<UEdGraphPin*> GatherOutputPins(const TArray<TWeakObjectPtr<UEdGraphNode>>& InNodes, const FEdGraphPinType& InSpecificPinType);
 
-	static int32 GetNodesMaxPositionX(const TArray<UEdGraphNode*>& InNodes);
-	static int32 GetNodesAveragePositionY(const TArray<UEdGraphNode*>& InNodes);
+	static bool TryGetNodesOutputPinsSameType(const TArray<TWeakObjectPtr<UEdGraphNode>>& InNodes, FEdGraphPinType& OutNodesOutputPinsSameType);
+	static bool IsExecutionPinType(const FEdGraphPinType& InPinType);
+	
+	static void SortPinsByPositionY(TArray<UEdGraphPin*>& OutPins);
+	static FIntPoint GetPinPosition(const UEdGraphPin* InPin);
+	static FIntPoint GetDesiredArrayNodePosition(const TArray<TWeakObjectPtr<UEdGraphNode>>& InNodes);
+	static int32 GetNodesMaxPositionX(const TArray<TWeakObjectPtr<UEdGraphNode>>& InNodes);
+	static int32 GetNodesAveragePositionY(const TArray<TWeakObjectPtr<UEdGraphNode>>& InNodes);
 };
