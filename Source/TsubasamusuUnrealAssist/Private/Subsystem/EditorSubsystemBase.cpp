519b1d5036aecd22e5eee2e9e11e0153bbd41fc6
tsubasamusu <tsubasa_03_08@icloud.com>
2026-03-22 07:43:38 +0900

    #59 UEditorSubsystemBase を定義

--- Source/TsubasamusuUnrealAssist/Private/Subsystem/EditorSubsystemBase.cpp
+++ Source/TsubasamusuUnrealAssist/Private/Subsystem/EditorSubsystemBase.cpp
@@ -1,3 +1,28 @@
 // Copyright (c) 2026, tsubasamusu All rights reserved.
 
 #include "EditorSubsystemBase.h"
+
+void UEditorSubsystemBase::Initialize(FSubsystemCollectionBase& InSubsystemCollectionBase)
+{
+	PostEngineInitHandle = FCoreDelegates::OnPostEngineInit.AddUObject(this, &UEditorSubsystemBase::OnPostEngineInit);
+}
+
+void UEditorSubsystemBase::Deinitialize()
+{
+	if (PostEngineInitHandle.IsValid())
+	{
+		FCoreDelegates::OnPostEngineInit.Remove(PostEngineInitHandle);
+	}
+	
+	if (AssetEditorOpenedHandle.IsValid() && IsValid(GEditor))
+	{
+		UAssetEditorSubsystem* AssetEditorSubsystem = GEditor->GetEditorSubsystem<UAssetEditorSubsystem>();
+		AssetEditorSubsystem->OnAssetEditorOpened().Remove(AssetEditorOpenedHandle);
+	}
+}
+
+void UEditorSubsystemBase::OnPostEngineInit()
+{
+	UAssetEditorSubsystem* AssetEditorSubsystem = GEditor->GetEditorSubsystem<UAssetEditorSubsystem>();
+	AssetEditorOpenedHandle = AssetEditorSubsystem->OnAssetEditorOpened().AddUObject(this, &UEditorSubsystemBase::OnAssetEditorOpened);
+}
--- Source/TsubasamusuUnrealAssist/Private/Subsystem/EditorSubsystemBase.h
+++ Source/TsubasamusuUnrealAssist/Private/Subsystem/EditorSubsystemBase.h
@@ -3,6 +3,27 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "EditorSubsystemBase.generated.h"
+
+UCLASS(Abstract)
+class UEditorSubsystemBase : public UEditorSubsystem
+{
+	GENERATED_BODY()
+	
+public:
+	//~ Begin USubsystem Interface
+	virtual void Initialize(FSubsystemCollectionBase& InSubsystemCollectionBase) override;
+	virtual void Deinitialize() override;
+	//~ End USubsystem Interface
+	
+protected:
+	virtual void OnPostEngineInit();
+	virtual void OnAssetEditorOpened(UObject* InOpenedAsset){}
+	
+private:
+	FDelegateHandle PostEngineInitHandle;
+	FDelegateHandle AssetEditorOpenedHandle;
+};
 
 template<typename DerivedClass>
 class TEditorSubsystemBase
