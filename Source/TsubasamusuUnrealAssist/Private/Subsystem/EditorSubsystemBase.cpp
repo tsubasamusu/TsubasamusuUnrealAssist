e62ac5ae779b043dee0477efb789cab521b96bc9
tsubasamusu <tsubasa_03_08@icloud.com>
2026-06-25 06:26:28 +0900

    #65 UE 5.8 以降で発生するコンパイルエラーに対処

--- Source/TsubasamusuUnrealAssist/Private/Subsystem/EditorSubsystemBase.cpp
+++ Source/TsubasamusuUnrealAssist/Private/Subsystem/EditorSubsystemBase.cpp
@@ -1,17 +1,32 @@
 // Copyright (c) 2026, tsubasamusu All rights reserved.
 
 #include "EditorSubsystemBase.h"
+#include "Type/TsubasamusuUnrealAssistMacros.h"
 
 void UEditorSubsystemBase::Initialize(FSubsystemCollectionBase& InSubsystemCollectionBase)
 {
-	PostEngineInitHandle = FCoreDelegates::OnPostEngineInit.AddUObject(this, &UEditorSubsystemBase::OnPostEngineInit);
+	FSimpleMulticastDelegate& PostEngineInitDelegate =
+#if UE_VERSION_NEWER_THAN_OR_EQUAL(5, 8, 0)
+		FCoreDelegates::GetOnPostEngineInit();
+#else
+		FCoreDelegates::OnPostEngineInit;
+#endif
+	
+	PostEngineInitHandle = PostEngineInitDelegate.AddUObject(this, &UEditorSubsystemBase::OnPostEngineInit);
 }
 
 void UEditorSubsystemBase::Deinitialize()
 {
 	if (PostEngineInitHandle.IsValid())
 	{
-		FCoreDelegates::OnPostEngineInit.Remove(PostEngineInitHandle);
+		FSimpleMulticastDelegate& PostEngineInitDelegate =
+#if UE_VERSION_NEWER_THAN_OR_EQUAL(5, 8, 0)
+			FCoreDelegates::GetOnPostEngineInit();
+#else
+			FCoreDelegates::OnPostEngineInit;
+#endif
+		
+		PostEngineInitDelegate.Remove(PostEngineInitHandle);
 	}
 	
 	if (AssetEditorOpenedHandle.IsValid() && IsValid(GEditor))
